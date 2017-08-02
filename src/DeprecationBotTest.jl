cb82f6118ace81e65282cb07427c203a29a2bcaf
Keno Fischer <keno@alumni.harvard.edu>
2017-08-02 13:31:36 -0400

    Add example from JLD

--- src/DeprecationBotTest.jl
+++ src/DeprecationBotTest.jl
@@ -4,4 +4,12 @@ function foobar{T}(x, y::T)
     println(x, y)
 end
 
+if !(isdefined(Core, :String) && isdefined(Core, :AbstractString))
+    function jlconvert(T::Union{Type{Compat.ASCIIString},Type{Compat.UTF8String}}, ::JldFile, ptr::Ptr)
+        strptr = unsafe_load(convert(Ptr{Ptr{UInt8}}, ptr))
+        n = Int(ccall(:strlen, Csize_t, (Ptr{UInt8},), strptr))
+        T(unsafe_wrap(Array, strptr, n, true))
+    end
+end
+
 end # module
