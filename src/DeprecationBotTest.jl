f642cf390a21d45a7ec9e53bd149357cb83f7ea6
Keno Fischer <keno@juliacomputing.com>
2018-06-03 21:19:49 -0400

    Update DeprecationBotTest.jl

--- src/DeprecationBotTest.jl
+++ src/DeprecationBotTest.jl
@@ -1,25 +1,9 @@
 module DeprecationBotTest
 
-function foobar(x, y::T) where T
-    println(x, y)
-end
-
-if !(isdefined(Core, :String) && isdefined(Core, :AbstractString))
-    function jlconvert(T::Union{Type{String},Type{String}}, ::JldFile, ptr::Ptr)
-        strptr = unsafe_load(convert(Ptr{Ptr{UInt8}}, ptr))
-        n = Int(ccall(:strlen, Csize_t, (Ptr{UInt8},), strptr))
-        T(unsafe_wrap(Array, strptr, n, true))
-    end
-end
-
-_foo(x) = foo(x)
-
-bar(x) = _foo(x)
-
-function foobar(a,
-            b::T,
-            c::T) where T
-    (a, b, c)    
+if VERSION >= v"0.1"
+    print("Hello")
+else
+    print("World")
 end
 
 end # module
